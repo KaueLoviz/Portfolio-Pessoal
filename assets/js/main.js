8b91b3cac5567b168121ab53d2f5c0a50bade357
Kauê Loviz de Oliveira <56116607+KaueLoviz@users.noreply.github.com>
2024-07-25 13:18:07 -0300

    corrigindo bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,27 @@
-/*==================== MENU SHOW(ATIVO) e HIDDEN(OCULTO) ====================*/
-const navMenu = document.getElementById('nav-menu'),
-      navToogle = document.getElementById('nav-toogle'),
-      navClose = document.getElementById('nav-close')
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
-
-/*===== MENU SHOW(ATIVO) =====*/
-/* Validano constantes existentes */
-if(navToogle){
-    navToogle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+/*===== MENU SHOW =====*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
-
-/*===== MENU HIDDEN(OCULTO) =====*/
-/* Validano constantes existes */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+/*===== MENU HIDDEN =====*/
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
-
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // Quando clicamos em cada nav__link, removemos as class show-menu
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const navLinks = document.querySelectorAll(".nav__link");
+navLinks.forEach((link) =>
+  link.addEventListener("click", () => navMenu.classList.remove("show-menu"))
+);
 
 /*==================== ACCORDION SKILLS ====================*/
 const skillsContent = document.querySelectorAll(".skills__content");
@@ -49,54 +40,46 @@ function toggleSkills() {
 skillsHeader.forEach((skillHeader) =>
   skillHeader.addEventListener("click", toggleSkills)
 );
+
 /*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+const tabs = document.querySelectorAll("[data-target]");
+const tabsContent = document.querySelectorAll("[data-content]");
 
-tabs.forEach(tab =>{
-    tab.addEventListener('click', () =>{
-        const target = document.querySelector(tab.dataset.target)
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
 
-        tabContents.forEach(tabContent =>{
-          tabContent.classList.remove('qualification__active')
-        })
-        target.classList.add('qualification__active')
+    tabsContent.forEach((tabContent) =>
+      tabContent.classList.remove("qualification__active")
+    );
+    target.classList.add("qualification__active");
 
-        tab.forEach(tab =>{
-          tab.classList.remove('qualification__active')
-        })
-        tab.classList.add('qualification__active')
-    }) 
-})      
+    tabs.forEach((tab) => tab.classList.remove("qualification__active"));
+    tab.classList.add("qualification__active");
+  });
+});
 
 /*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.services__button'),
-      modalClose = document.querySelectorAll('.services__modal-close')
+const btnsOpenModal = document.querySelectorAll(".services__button");
+const btnsCloseModal = document.querySelectorAll(".services__modal-close");
+const modals = document.querySelectorAll(".services__modal");
 
-let modal = function(modalClick){
-  modalViews[modalClick].classList.add('active-modal')
-}
+btnsOpenModal.forEach((openModal, i) => {
+  openModal.addEventListener("click", () => {
+    modals[i].classList.add("active-modal");
+  });
+});
 
-modalBtns.forEach((modalBtn, i) =>{
-  modalBtn.addEventListener('click', () =>{
-    modal(i)
-  })
-})
-
-modalClose.forEach((modalClose) =>{
-  modalClose.addEventListener('click', () =>{
-    modalViews.forEach((modalView) =>{
-      modalView.classList.remove('active-modal')
-    })
-  })
-})
+btnsCloseModal.forEach((closeModal, i) => {
+  closeModal.addEventListener("click", () => {
+    modals[i].classList.remove("active-modal");
+  });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiperPorfolio = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper(".portfolio__container", {
   cssMode: true,
   loop: true,
-
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -105,46 +88,52 @@ let swiperPorfolio = new Swiper('.portfolio__container', {
     el: ".swiper-pagination",
     clickable: true,
   },
+  mousewheel: true,
+  keyboard: true,
 });
 
 /*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper('.testimonial__container', {
+let swiperTestimonial = new Swiper(".testimonial__container", {
   loop: true,
   grabCursor: true,
   spaceBetween: 48,
-
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
   },
-  breakpoints:{
-    568:{
-      slidesPerView: 1,
-    }
-  }
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
 
-function scrollActive(){
-  const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-  sections.forEach(current => {
+  sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
     sectionId = current.getAttribute("id");
 
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link")
-    }else{
-      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link")
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.nav__menu a[href*=${sectionId}]`)
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(`.nav__menu a[href*=${sectionId}]`)
+        .classList.remove("active-link");
     }
-  })
+  });
 }
-window.addEventListener("scroll", scrollActive)
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+window.addEventListener("scroll", scrollActive);
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
   const nav = document.getElementById("header");
   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
@@ -153,7 +142,7 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
@@ -162,7 +151,7 @@ function scrollUp() {
 }
 window.addEventListener("scroll", scrollUp);
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "uil-sun";
